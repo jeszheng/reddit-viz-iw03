7d5d07de59df83f31e6c513d7fec43bad1943a5e
Jessica Zheng <jessica.zheng.2015@gmail.com>
2017-10-13 23:11:01 -0400

    Added Date Picker UI Element, Data Collection 10/13

--- import.py
+++ import.py
@@ -17,7 +17,7 @@ def html_escape(text):
 def main():
     # file name.
     allPosts = []
-    date = "20171012"
+    date = "20171013"
     # don't forget to comment out database url in models file!
 
     ################################################################################
