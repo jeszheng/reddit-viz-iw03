e94a75a56c24d8e6e93fc88843e882e1121ec65c
Jessica Zheng <jessica.zheng.2015@gmail.com>
2017-10-29 15:32:14 -0700

    Added new model schema

---
+++ import_new_nocomments.py
@@ -0,0 +1,190 @@
+# from models import TopPost, ControversialPost, db
+import json
+import sys
+
+html_escape_table = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;",
+    ">": "&gt;",
+    "<": "&lt;",
+    }
+
+def html_escape(text):
+    # """Produce entities within text."""
+    return "".join(html_escape_table.get(c,c) for c in text)
+
+def main():
+    # file name.
+    allPosts = []
+    date = "20171028"
+    # TODO conditional reading from extra commenting process.
+    # don't forget to comment out database url in models file!
+
+    ################################################################################
+
+    # Insert in top post data from specified date.
+
+    ################################################################################
+    with open(
+    '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/' + date + '_top.json') as data_file:
+        allPosts = json.load(data_file)
+
+    #Load title pos/neg sentiments
+    with open(
+    '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/post-processing/jsonfiles/top_title_sentiment_' + date + '.json') as data_file:
+        t_titlePosNegSentiments = json.load(data_file)
+
+    # Load comments pos/neg sentiments (all subreddits)
+    with open(
+    '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/post-processing/jsonfiles/comments_posneg_sentiment_' + date + '.json') as data_file:
+        commentsPosNegSentiments = json.load(data_file)
+
+    # # Load comments poltical sentiments (for r/politics only)
+    # with open(
+    # '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/post-processing/jsonfiles/comments_political_sentiment_' + date + '.json') as data_file:
+    #     commentsPoliticalSentiments = json.load(data_file)
+
+    # Only use posts in subs poltics, news, worldnews, technology
+    selectPosts = []
+    for post in allPosts:
+        if post['subreddit'] == 'politics' or post['subreddit'] == 'news' or post['subreddit'] == 'worldnews' or post['subreddit'] == 'technology':
+            selectPosts.append(post)
+
+    for post in selectPosts:
+        # filter out pinned or stickied posts.
+        if post['stickied'] or post['over_18']:
+            continue
+
+        # Title Pos/Neg Sentiment processing.
+        sentiment_compound = 0
+        found = False
+        for titleSentiment in t_titlePosNegSentiments:
+            if titleSentiment['id'] == post['id']:
+                sentiment_compound = titleSentiment['sentiment_compound']
+                found = True
+                break
+        if not found:
+            print 'Top title sentiment not found!' # should never occur
+
+        # Comment Pos/Neg Sentiment processing.
+        # NOT DONE FOR DATES BEFORE 10/1
+        tc_strongly_pos = -1
+        tc_pos = -1
+        tc_neu = -1
+        tc_neg = -1
+        tc_strongly_neg = -1
+        cc_strongly_pos = -1
+        cc_pos = -1
+        cc_neu = -1
+        cc_neg = -1
+        cc_strongly_neg = -1
+
+        # Comment Political Sentiment processing for r/politics.
+        # NOT DONE FOR DATES BEFORE 10/1
+        tc_libertarian_avg = float(-1)
+        tc_conservative_avg = float(-1)
+        tc_liberal_avg = float(-1)
+        cc_libertarian_avg = float(-1)
+        cc_conservative_avg = float(-1)
+        cc_liberal_avg = float(-1)
+
+        topPost = TopPost(post['id'],
+            date,
+            post['permalink'],
+            post['url'],
+            html_escape(post['title']),
+            html_escape(post['selftext']),
+            post['author_link_karma'],
+            post['subreddit'],
+            post['score'],
+            post['upvote_ratio'],
+            post['num_comments'],
+            post['libertarian'],
+            post['green'],
+            post['liberal'],
+            post['conservative'],
+            sentiment_compound,
+            tc_strongly_pos,
+            tc_pos,
+            tc_neu,
+            tc_neg,
+            tc_strongly_neg,
+            cc_strongly_pos,
+            cc_pos,
+            cc_neu,
+            cc_neg,
+            cc_strongly_neg,
+            tc_libertarian_avg,
+            tc_conservative_avg,
+            tc_liberal_avg,
+            cc_libertarian_avg,
+            cc_conservative_avg,
+            cc_liberal_avg)
+
+        db.session.add(topPost)
+
+    db.session.commit()
+    print 'committed top posts from', date
+
+    ################################################################################
+
+    # Insert in controversial post data from specified date.
+
+    ################################################################################
+
+    with open(
+    '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/' + date + '_controversial.json') as data_file:
+        allPosts = json.load(data_file)
+
+    # Load title pos/neg sentiments
+    with open(
+    '/Users/jessicazheng/Documents/Academics/2017-2018/IW3/reddit-viz-iw03/data_collection/post-processing/jsonfiles/controversial_title_sentiment_' + date + '.json') as data_file:
+        c_titlePosNegSentiments = json.load(data_file)
+
+    selectPosts = []
+    for post in allPosts:
+        if post['subreddit'] == 'politics' or post['subreddit'] == 'news' or post['subreddit'] == 'worldnews' or post['subreddit'] == 'technology':
+            selectPosts.append(post)
+
+    # Only use posts in subs poltics, news, worldnews, technology
+
+    for post in selectPosts:
+        # filter out pinned or stickied posts.
+        if post['stickied'] or post['over_18']:
+            continue
+
+        # Title Pos/Neg Sentiment processing.
+        sentiment_compound = 0
+        found = False
+        for titleSentiment in c_titlePosNegSentiments:
+            if titleSentiment['id'] == post['id']:
+                sentiment_compound = titleSentiment['sentiment_compound']
+                found = True
+                break
+        if not found:
+            print 'Top title sentiment not found!', post['id'], '', post['title'] # should never occur
+
+        controversialPost = ControversialPost(
+            post['id'],
+            date,
+            post['permalink'],
+            post['url'],
+            html_escape(post['title']),
+            html_escape(post['selftext']),
+            post['author_link_karma'],
+            post['subreddit'],
+            post['score'],
+            post['upvote_ratio'],
+            post['num_comments'],
+            post['libertarian'],
+            post['green'],
+            post['liberal'],
+            post['conservative'])
+        db.session.add(controversialPost)
+
+    db.session.commit()
+    print 'committed controversial posts from ', date
+
+if __name__ == '__main__':
+    main()
--- models_old.py
+++ models_old.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 # TODO COMMENT OUT LINE BELOW BEFORE PUSH
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/devel_db'
 
-# purple
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cmodmuptjjyklg:e48f9a96060da864807bd5b967ea0447fd5c4814a7583facde3afd9d729726ce@ec2-184-72-248-8.compute-1.amazonaws.com:5432/dbogg3844cnn32'
+# OLIVE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://shbcgushorflvj:1cb55e3f68ba432e61899150acaf36a66e283cf43f619c3d4fd930382aec8a12@ec2-204-236-236-188.compute-1.amazonaws.com:5432/dcpd22ounv85v6'
 
+# BLACK
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cqvjbobiquqase:a7d4d05d62c673ed79207cd44c9ae86573c164871b6c26e6b46bed410624295e@ec2-54-221-221-153.compute-1.amazonaws.com:5432/dac5ce63jaaa4s'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -33,25 +34,9 @@ class TopPost(db.Model):
     green = db.Column(db.Float)
     liberal = db.Column(db.Float)
     conservative = db.Column(db.Float)
-    sentiment_compound = db.Column(db.Float)
-    tc_strongly_pos = db.Column(db.Integer)
-    tc_pos = db.Column(db.Integer)
-    tc_neu = db.Column(db.Integer)
-    tc_neg = db.Column(db.Integer)
-    tc_strongly_neg = db.Column(db.Integer)
-    cc_strongly_pos = db.Column(db.Integer)
-    cc_pos = db.Column(db.Integer)
-    cc_neu = db.Column(db.Integer)
-    cc_neg = db.Column(db.Integer)
-    cc_strongly_neg = db.Column(db.Integer)
-    tc_libertarian_avg = db.Column(db.Float)
-    tc_conservative_avg = db.Column(db.Float)
-    tc_liberal_avg = db.Column(db.Float)
-    cc_libertarian_avg = db.Column(db.Float)
-    cc_conservative_avg = db.Column(db.Float)
-    cc_liberal_avg = db.Column(db.Float)
+    submission_time = db.Column(db.String())
 
-    def __init__(self, post_id, date, permalink, url, title, selftext, author_link_karma, subreddit, score, upvote_ratio, num_comments, libertarian, green, liberal, conservative, sentiment_compound, tc_strongly_pos, tc_pos, tc_neu, tc_neg, tc_strongly_neg, cc_strongly_pos, cc_pos, cc_neu, cc_neg, cc_strongly_neg, tc_libertarian_avg, tc_conservative_avg, tc_liberal_avg, cc_libertarian_avg, cc_conservative_avg, cc_liberal_avg):
+    def __init__(self, post_id, date, permalink, url, title, selftext, author_link_karma, subreddit, score, upvote_ratio, num_comments, libertarian, green, liberal, conservative, submission_time):
         self.post_id = post_id
         self.date = date
         self.permalink = permalink
@@ -67,27 +52,10 @@ class TopPost(db.Model):
         self.green = green
         self.liberal = liberal
         self.conservative = conservative
-        self.sentiment_compound = sentiment_compound
-        self.tc_strongly_pos = tc_strongly_pos
-        self.tc_pos = tc_pos
-        self.tc_neu = tc_neu
-        self.tc_neg = tc_neg
-        self.tc_strongly_neg = tc_strongly_neg
-        self.cc_strongly_pos = cc_strongly_pos
-        self.cc_pos = cc_pos
-        self.cc_neu = cc_neu
-        self.cc_neg = cc_neg
-        self.cc_strongly_neg = cc_strongly_neg
-        self.tc_libertarian_avg = tc_libertarian_avg
-        self.tc_conservative_avg = tc_conservative_avg
-        self.tc_liberal_avg = tc_liberal_avg
-        self.cc_libertarian_avg = cc_libertarian_avg
-        self.cc_conservative_avg = cc_conservative_avg
-        self.cc_liberal_avg = cc_liberal_avg
+        self.submission_time = submission_time
 
     def __repr__(self):
         return '<p>%r</p>' % (self.post_id)
-
 
 class ControversialPost(db.Model):
     __tablename__ = "controversialPosts"
@@ -107,9 +75,9 @@ class ControversialPost(db.Model):
     green = db.Column(db.Float)
     liberal = db.Column(db.Float)
     conservative = db.Column(db.Float)
-    sentiment_compound = db.Column(db.Float)
+    submission_time = db.Column(db.String())
 
-    def __init__(self, post_id, date, permalink, url, title, selftext, author_link_karma, subreddit, score, upvote_ratio, num_comments, libertarian, green, liberal, conservative, sentiment_compound):
+    def __init__(self, post_id, date, permalink, url, title, selftext, author_link_karma, subreddit, score, upvote_ratio, num_comments, libertarian, green, liberal, conservative, submission_time):
         self.post_id = post_id
         self.date = date
         self.permalink = permalink
@@ -125,7 +93,7 @@ class ControversialPost(db.Model):
         self.green = green
         self.liberal = liberal
         self.conservative = conservative
-        self.sentiment_compound = sentiment_compound
+        self.submission_time = submission_time
 
     def __repr__(self):
         return '<p>%r</p>' % (self.post_id)
